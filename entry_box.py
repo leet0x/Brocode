4fb3a9ce8ccea7ac8649330bc13a08fc7816ee90
emri <email>
2022-09-15 17:55:08 +0200

    added new file entry_box

---
+++ entry_box.py
@@ -0,0 +1,39 @@
+from tkinter import *
+
+# entry widget = textbox that accepts a single line  of user input
+
+
+def submit():
+    username = entry.get()
+    print("hello " + username)
+
+
+def delete():
+    entry.delete(0, END)
+
+
+def backspace():
+    entry.delete(len(entry.get())-1, END)
+
+
+window = Tk()
+
+entry = Entry(window,
+              font=("Arial", 50),
+              fg="#00FF00",
+              bg="Black")
+entry.insert(1, "Batman")
+# entry.config()
+entry.pack(side=LEFT)
+
+submit_button = Button(window, text="Submit", command=submit)
+submit_button.pack(side=RIGHT)
+
+delete_button = Button(window, text="Delete", command=delete)
+delete_button.pack(side=RIGHT)
+
+backspace_button = Button(window, text="Backspace", command=backspace)
+backspace_button.pack(side=RIGHT)
+
+window.mainloop()
+
--- test_git.py
+++ test_git.py
@@ -2,3 +2,4 @@ var = 5
 var2 = 0
 var3 = 6
 print("git test", var)
+var4 = "new feature"
