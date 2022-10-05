513a6ae09a3a913198165178e34dccd4a75ce28d
emri <email>
2022-10-05 16:00:42 +0200

    added tennis ball

--- GUI_multiple_animations/GUI_multiple_animations.py
+++ GUI_multiple_animations/GUI_multiple_animations.py
@@ -1,5 +1,23 @@
 from tkinter import *
+from Ball import *
 import time
 
 window = Tk()
+window.title("GUI Multiple Balls animation")
+
+WIDTH = 500
+HEIGHT = 500
+
+canvas = Canvas(window,width=WIDTH,height=HEIGHT)
+canvas.pack()
+
+volley_ball = Ball(canvas,0,0,100,1,1,"white")
+
+while True:
+    volley_ball.move()
+    window.update()
+    time.sleep(0.01)
+
+
+
 window.mainloop()
