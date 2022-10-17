4c2a047c6c26789a8a0089fe79f448854c786aab
emri <email>
2022-10-17 17:10:12 +0200

    did some exercises

---
+++ exercises/Basic Exercise for Beginners/compare_first_last_nr.py
@@ -0,0 +1,7 @@
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+if numbers_y[0] == numbers_x[len(numbers_y)-1]:
+    print(True)
+else:
+    print(False)
---
+++ exercises/loop/pattern1.py
@@ -0,0 +1,12 @@
+# Write a program to print the following number pattern using a loop.
+#
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+for row in range(1, 6):
+    for col in range(1, row+1):
+        print(col, end=" ")
+    print()
