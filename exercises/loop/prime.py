772622c3304d47a99616748834933d8ed8074495
emri <email>
2022-10-18 16:21:43 +0200

    added fibonacci seq

--- exercises/loop/prime.py
+++ exercises/loop/prime.py
@@ -1,17 +1,15 @@
 def generatePrimeNumbers(start=0, end=0):
     prime_numbers = []
     for nr in range(start, end):
-        if nr > 0:
+        if nr > 1:
             for i in range(2, nr):
                 if nr % i == 0:
-                    # not a prime number so break inner loop
                     break
                 else:
                     if nr not in prime_numbers:
                         prime_numbers.append(nr)
-
     return prime_numbers
 
 
-prime_number = generatePrimeNumbers(25, 50)
+prime_number = generatePrimeNumbers(start=25, end=50)
 print(prime_number)
