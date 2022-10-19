c48ee27ba1a2f0e3f58ae5a7f7c8ef1123efb85e
nga puna <email>
2022-10-19 10:53:16 +0200

    added reverse and modify functions

--- exercises/loop/prime.py
+++ exercises/loop/prime.py
@@ -11,5 +11,21 @@ def generatePrimeNumbers(start=0, end=0):
     return prime_numbers
 
 
+def reverse(prime_numbers):
+    return prime_numbers[::-1]
+
+
+def modify(prime_numbers):
+    prime_numbers[:3] = ["one", "two", "three"]
+    return prime_numbers
+
+
+def
+
+
 prime_number = generatePrimeNumbers(start=25, end=50)
 print(prime_number)
+print(reverse(prime_number))
+print(modify(prime_number))
+
+
