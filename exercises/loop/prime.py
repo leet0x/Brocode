2b52eac70e0265a82f63eaf2bc3964f733d72c4e
nga puna <email>
2022-10-19 15:27:34 +0200

    added list methods

--- exercises/loop/prime.py
+++ exercises/loop/prime.py
@@ -11,7 +11,8 @@ def generatePrimeNumbers(start=0, end=0):
     return prime_numbers
 
 
-def reverse(prime_numbers):
+def reverse_list(prime_numbers):
+    # prime_numbers.reverse()
     return prime_numbers[::-1]
 
 
@@ -20,12 +21,35 @@ def modify(prime_numbers):
     return prime_numbers
 
 
-def
+def delete(prime_numbers):
+    del prime_numbers[:3]
+    return prime_numbers
+
+
+def sort_list(prime_numbers):
+    prime_numbers.sort()
+    # prime_numbers.sort(reverse=True)
+    return prime_numbers
+
+
+def extend_list(current_list=[], to_add_list = []):
+    current_list.extend(to_add_list)
+    return current_list
 
 
 prime_number = generatePrimeNumbers(start=25, end=50)
 print(prime_number)
-print(reverse(prime_number))
+print(reverse_list(prime_number))
 print(modify(prime_number))
-
-
+print(delete(prime_number))
+# sort_list(prime_number)
+print(sort_list(prime_number))
+prime_number *=2
+print(prime_number)
+print(prime_number.index(33, 2))
+to_add = ["red", "yellow", "orange"]
+print(extend_list(prime_number, to_add))
+to_add.remove("yellow")
+print(to_add)
+to_add.clear()
+print(to_add)
