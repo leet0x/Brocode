d4f3b3e00002e9f467c39bed9330603396b57cf5
nga puna <email>
2022-10-20 12:29:25 +0200

    list and tuples functions

--- exercises/loop/prime.py
+++ exercises/loop/prime.py
@@ -53,3 +53,40 @@ to_add.remove("yellow")
 print(to_add)
 to_add.clear()
 print(to_add)
+
+#list comprehension
+# so instead of writing a for loop for creating a list with some generated items
+# we can just write the following
+# list = [item for item in range(1, 6)]
+# print(list)
+# OR even shorter
+list = list(range(1, 6))
+print(list)
+
+# filtering list comprehension with if clauses
+filtered_list = [item for item in range(1, 6) if item % 2 == 0]
+
+print(filtered_list)
+
+# list comprehension that processes another list's elements
+color = ["red", "orange", "blue", "white", "black"]
+color2 = [item.upper() for item in color]
+print(color2)
+
+
+names = ["Bob", "Sue", "Amanda"]
+grade_point_averages = [3.5, 4.0, 3.75]
+# zipped = zip(names, grade_point_averages)
+for name, gpa in zip(names, grade_point_averages):
+    print(f"{name}: {gpa}")
+
+# Two dimensional lists
+grades = [[77, 68, 86, 73],
+          [96, 87, 89, 81],
+          [70, 90, 86, 81]]
+for row in grades:
+    # print(row)
+    for item in row:
+        print(item, end=" ")
+    print()
+
