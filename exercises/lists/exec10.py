af1cc4ae13d8e2d3e8cbffb8f075474d48c0d79b
nga puna <email>
2022-10-25 17:16:48 +0200

    did some exercises 9, 10

---
+++ exercises/lists/exec10.py
@@ -0,0 +1,20 @@
+# Exercise 10: Remove all occurrences of a specific item from a list.
+# Given a Python list, write a program to remove all occurrences of
+# item 20.
+
+# Given:
+list1 = [5, 20, 15, 20, 25, 50, 20]
+
+# Expected output:
+# [5, 15, 25, 50]
+
+# solution 1
+# list2 = list(filter(lambda x: x != 20, list1))
+# print(list2)
+
+# solution 2
+# while 20 in list1:
+#     list1.remove(20)
+# print(list1)
+
+# solution 3
